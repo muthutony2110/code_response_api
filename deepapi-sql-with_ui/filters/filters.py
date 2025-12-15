f1195c43033f73a623a58674303ca56905a4c476
muthutony2110 <115693674+muthutony2110@users.noreply.github.com>
2025-12-15 18:58:38 +0530

    Delete deepapi-sql-with_ui directory

--- deepapi-sql-with_ui/filters/filters.py
+++
@@ -1,3 +0,0 @@
-def is_greeting(prompt):
-    greetings = ["hi", "hello", "hey", "good morning", "good evening", "good afternoon"]
-    return any(prompt.lower().strip().startswith(greet) for greet in greetings)
